e2cc5db2c4d63cbcde5bcfbbd7d9fe512a6f2b2c
bug <ssilvass@protonmail.com>
2022-11-27 16:52:29 -0300

    Primeiras Funcionalidades

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+function calculo(bimestreum, bimestredois, bimestretreis, bimestrequatro, media){
+	bimestreum = Number.parseFloat(b1.value)
+	bimestredois = Number.parseFloat(b2.value)
+	bimestretreis = Number.parseFloat(b3.value)
+	bimestrequatro = Number.parseFloat(b4.value)
+	media = (bimestreum + bimestredois + bimestretreis + bimestrequatro)
+	alert("Sua média final é: " + media)
+	/* if(media >= 24){
+		alert("Voce passou!")
+	}else{
+		alert("Voce foi reprovado!")
+	} */
+}
