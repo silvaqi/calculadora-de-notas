d68068fb214b9302b9e08593ed9eba8969e8bd86
bug <ssilvass@protonmail.com>
2022-12-17 08:53:18 -0300

    Nova versão CSS e JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,1 @@
-function calculo(bimestreum, bimestredois, bimestretreis, bimestrequatro, media){
-	bimestreum = Number.parseFloat(bimestre1h.value)
-	bimestredois = Number.parseFloat(bimestre2h.value)
-	bimestretreis = Number.parseFloat(bimestre3h.value)
-	bimestrequatro = Number.parseFloat(bimestre4h.value)
-	media = (bimestreum + bimestredois + bimestretreis + bimestrequatro)
-	alert("Sua média final é: " + media)
-}
+
