9e5d205f96bfba67c70f2973c62c175832370c53
bug <ssilvass@protonmail.com>
2022-12-17 17:29:23 -0300

    New version

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-
+function calculo(numero1, numero2, numero3, numero4, soma){
+	numero1 = Number.parseFloat(nota1.value)
+	numero2 = Number.parseFloat(nota2.value)
+	numero3 = Number.parseFloat(nota3.value)
+	numero4 = Number.parseFloat(nota4.value)
+	soma = (numero1 + numero2 + numero3 + numero4)
+	if(soma >= 24){
+		alert("Voce passou! Sua média final é: " + soma)
+	}else{
+		alert("Voce não passou! Sua média final é: " + soma)
+	}
+}
